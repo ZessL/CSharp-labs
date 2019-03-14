0d1edabe1661989374efb7ecebf969f1c127d525
ZessL <vfilenko3@gmail.com>
2019-03-14 11:57:19 +0200

    cs labs

---
+++ lab1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cs_lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number");
+            string numb = Console.ReadLine();
+            int number = Convert.ToInt32(numb);
+            int outn = Kube(number);
+            Console.WriteLine($"Число {numb}, возведённое в 3 степень: {outn}");
+            Console.ReadKey();
+        }
+        static int Kube(int number) {
+            int knumb = number * number * number;
+            return knumb;
+        }
+    }
+}
